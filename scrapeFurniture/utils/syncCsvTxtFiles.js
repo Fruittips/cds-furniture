ecd2aae3b40febe62c944b88e296f21b36496d9a
Fruittips <yida_mah@mymail.sutd.edu.sg>
2024-02-17 16:42:16 +0800

    update sync file script

--- scrapeFurniture/utils/syncCsvTxtFiles.js
+++ scrapeFurniture/utils/syncCsvTxtFiles.js
@@ -35,42 +35,53 @@ const syncFiles = (categories) => {
             "processedUrls_temp.txt"
         );
 
+        console.log("-----------------------------------");
+
+        console.log(`Syncing files for category: ${category}`);
+
         const urlsToKeep = new Set();
 
-        fs.createReadStream(csvFilePath)
-            .pipe(csv.parse({ headers: true }))
-            .on("error", (error) => console.error(error))
-            .on("data", (row) => {
-                if (row.title && row.title.trim() !== `""`) {
-                    urlsToKeep.add(row.url);
-                }
-            })
-            .on("end", () => {
-                console.log(`Finished filtering CSV.`);
+        try {
+            fs.createReadStream(csvFilePath)
+                .on("error", (error) => {
+                    throw new Error("Error reading CSV file for", category);
+                })
+                .pipe(csv.parse({ headers: true }))
+                .on("data", (row) => {
+                    if (row.title && row.title.trim() !== `""`) {
+                        urlsToKeep.add(row.url);
+                    }
+                })
+                .on("end", () => {
+                    console.log(`Finished filtering CSV.`);
 
-                fs.writeFileSync(tempTxtFilePath, [...urlsToKeep].join("\n"));
-                console.log("Temporary TXT file with URLs created.");
+                    fs.writeFileSync(tempTxtFilePath, [...urlsToKeep].join("\n"));
+                    console.log("Temporary TXT file with URLs created.");
 
-                const writeStream = fs.createWriteStream(tempCsvFilePath);
-                fs.createReadStream(csvFilePath)
-                    .pipe(csv.parse({ headers: true }))
-                    .pipe(csv.format({ headers: true }))
-                    .transform((row) => (urlsToKeep.has(row.url) ? row : null))
-                    .pipe(writeStream)
-                    .on("finish", () => {
-                        console.log("Temporary filtered CSV created.");
+                    const writeStream = fs.createWriteStream(tempCsvFilePath);
+                    fs.createReadStream(csvFilePath)
+                        .pipe(csv.parse({ headers: true }))
+                        .pipe(csv.format({ headers: true }))
+                        .transform((row) => (urlsToKeep.has(row.url) ? row : null))
+                        .pipe(writeStream)
+                        .on("finish", () => {
+                            console.log("Temporary filtered CSV created.");
 
-                        // Step 2: Delete original files
-                        fs.unlinkSync(csvFilePath);
-                        fs.unlinkSync(txtFilePath);
-                        console.log("Original files deleted.");
+                            // Step 2: Delete original files
+                            fs.unlinkSync(csvFilePath);
+                            fs.unlinkSync(txtFilePath);
+                            console.log("Original files deleted.");
 
-                        // Step 3: Rename new files to original names
-                        fs.renameSync(tempCsvFilePath, csvFilePath);
-                        fs.renameSync(tempTxtFilePath, txtFilePath);
-                        console.log("Temporary files renamed to original file names.");
-                    });
-            });
+                            // Step 3: Rename new files to original names
+                            fs.renameSync(tempCsvFilePath, csvFilePath);
+                            fs.renameSync(tempTxtFilePath, txtFilePath);
+                            console.log("Temporary files renamed to original file names.");
+                        });
+                });
+        } catch (error) {
+            console.log(error);
+            continue;
+        }
     }
 };
 
